e2ce525cfc1a2a6b36ed9b0c71b8266cd7b25536
rajan0031 <109027109+rajan0031@users.noreply.github.com>
2024-07-13 17:08:06 +0530

    this is the mongo commit

--- index.js
+++ index.js
@@ -138,13 +138,30 @@ app.use('/', AddPostsToUserProfileRoutes);
 // start of the atlas database connections
 
 
-mongoose.connect("mongodb+srv://raykushwaha0031:C1k4maJXzH2vAmh4@blog.zlf5agh.mongodb.net/Blog").then(() => {
-    console.log("Database connected succesfully")
-}).catch((err) => {
-    console.log(err)
-});
+// mongoose.connect("mongodb+srv://raykushwaha0031:C1k4maJXzH2vAmh4@blog.zlf5agh.mongodb.net/Blog").then(() => {
+//     console.log("Database connected succesfully")
+// }).catch((err) => {
+//     console.log(err)
+// });
 
 // end of the atlas database connections
+
+
+// start of the new way from the chatgpt
+
+mongoose.connect("mongodb+srv://raykushwaha0031:C1k4maJXzH2vAmh4@blog.zlf5agh.mongodb.net/Blog", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 10000,  // Example: Increased timeout to 10 seconds
+    socketTimeoutMS: 45000,           // Example: Increased socket timeout to 45 seconds
+}).then(() => {
+    console.log("Database connected successfully");
+}).catch((err) => {
+    console.error("Error connecting to database:", err);
+});
+
+
+//e nd of the new way from the chat gpt
 
 
 app.get("/", (req, res) => {
